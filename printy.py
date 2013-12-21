0907ffee18eeb521f1978e5d7539d3941f57635b
alcesleo <lagginglion@gmail.com>
2013-12-21 17:06:16 +0100

    Exclude patterns for files and folders

--- printy.py
+++ printy.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
 
 """
-Prints all contents of files in a directory to code-fenced markdown!
-To put it in a file do ./printy.py directory > hello.md
-
-Requires docopt to be installed.
-
 Usage:
     printy.py <directory>
+    printy.py <directory> --exclude=<folder_or_file_pattern>...
+
+Options:
+    -h --help     Show this screen.
+    --version     Show version.
+    --exclude=<glob> Exclude file or folder, for example --exclude='*.pyc'.
 
 """
 
@@ -15,17 +16,24 @@ import sys, os, re, fnmatch
 from textwrap import indent
 from docopt import docopt
 
-# TODO: as params
-excludes = ['.git', '*.sublime-*', '*.md']
-# transform glob patterns to regular expressions
-excludes = r'|'.join([fnmatch.translate(x) for x in excludes])
+def _regexify_globs(patterns):
+    """Turn a list of glob patterns into a regex."""
+    # http://stackoverflow.com/questions/5141437/filtering-os-walk-dirs-and-files
+    return r'|'.join([fnmatch.translate(x) for x in patterns]) # to regex
 
-def printify(directory):
+
+def printify(directory, excludes=None):
     """
     Prints contents of all files in a directory to stdout in
     simple markdown format. Every file gets a heading with its
     filename, and all content is indented as markdown code blocks.
     """
+
+    # standard excludes
+    if not excludes:
+        excludes = ['.git', '*.sublime-*', '*.md']
+
+    excludes = _regexify_globs(excludes)
 
     # output header
     print('# {0}\n'.format(directory))
@@ -43,7 +51,10 @@ def printify(directory):
 
             # print file with header
             print('## {0}\n'.format(path))
-            with open(path, 'r') as f:
+
+            # http://docs.python.org/3.3/library/functions.html#open
+            with open(path, 'r', errors='ignore') as f:
+                # todo ignore binary files
                 text = f.read()
                 # indent non-empty lines with 4 spaces
                 print(indent(text, '    ', lambda line: True))
@@ -52,4 +63,4 @@ def printify(directory):
 if __name__ == '__main__':
     args = docopt(__doc__, version='0.1')
 
-    printify(args['<directory>'])
+    printify(args['<directory>'], args['--exclude'])
